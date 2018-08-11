d07169604a7769a539d648ec303b84e390100f98
ykhwong <ykhwong@daum.net>
2018-08-12 08:57:04 +0900

    [FIX] Prevents arrow keys and space bar on window

--- frontend/client.js
+++ frontend/client.js
@@ -336,5 +336,11 @@ $(document).ready(function() {
 		return false;
 	},false);
 
+	window.addEventListener("keydown", function(e) {
+		if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+			e.preventDefault();
+		}
+	}, false);
+
 	init_imgpicker();
 });
