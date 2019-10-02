3360f764477c6a5ad217d48ecb89a03f15c4a954
ted <ykhwong@daum.net>
2019-10-03 07:55:57 +0900

    [FIX] atom-shell updated to 6.0.11

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERINFO=6.1.0
+set VERINFO=6.0.11
 set OPT=--icon=.\icon.ico --platform=win32 --overwrite --asar --app-copyright="MIT License (github.com/ykhwong/ppt-ndi)"
 
 call electron-packager ./ppt-ndi ppt-ndi --electron-version=%VERINFO% %OPT%
@@ -8,7 +8,6 @@ cd ppt-ndi-win32-x64
 del /q d3dcompiler_47.dll
 rmdir /s /q .\swiftshader
 rmdir /s /q .\locales
-rmdir /s /q .\resources\inspector
 del /q LICENSE
 del /q LICENSES.chromium.html
 del /q osmesa.dll 2>nul
@@ -17,9 +16,8 @@ del /q libGLESv2.dll
 del /q chrome_*.pak
 del /q snapshot_blob.bin
 del /q version
-mkdir bin
 copy ..\ffmpeg.dll .\.
-copy ..\PPTNDI.EXE .\bin\.
+copy ..\PPTNDI.dll .\.
 copy ..\Processing.NDI.Lib.x64.dll .\.
 copy ..\uiohook.dll .\.
 copy ..\*_slide.png .\resources\.
