aad4edb76a7cc320c1c8a9d4e07c9ea1a6ea8c32
ted <ykhwong@daum.net>
2019-10-23 23:33:01 +0900

    [FIX] npmBuild script

--- scripts/npmBuild.bat
+++ scripts/npmBuild.bat
@@ -2,8 +2,10 @@
 if not exist package.json goto err
 echo [Installation]
 CMD /C npm install --force --save 2>nul
+rmdir /s /q .\node_modules\ffi\node_modules\ref 2>nul
 rmdir /s /q .\node_modules\ffi\node_modules\ref-struct 2>nul
 rmdir /s /q .\node_modules\ref-struct\node_modules\ref 2>nul
+mklink /J .\node_modules\ffi\node_modules\ref .\node_modules\ref
 mklink /J .\node_modules\ffi\node_modules\ref-struct .\node_modules\ref-struct
 mklink /J .\node_modules\ref-struct\node_modules\ref .\node_modules\ref
 echo [Rebuild]
